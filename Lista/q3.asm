c840b626f8b5e503140a9dfafb948dca69eabbdb
Gabriel Santos <77673388+GansoHub@users.noreply.github.com>
2023-03-06 16:13:18 -0300

    questao3_fib

--- Lista/q3.asm
+++ Lista/q3.asm
@@ -1,36 +1,22 @@
-# [BCC 2022.1] Arquitetura e Organização de Computadores
-# Lista de exercícios - Projeto 01
-# Questão 01
-# Arquitetantes:
-# - Gabriel Santos
-# - Gilvaney Leandro
-# - Joyce Mirelle
-# - Ronaldo Rodrigues
-
-
-#O objetivo desta questão é treinar a implementação de funções — particularmente, 
-#funções recursivas. A sequência de Fibonacci têm os elementos 
-#F0 = 0 F1 = 1 e 
-#Fn = Fn-1 + Fn-2 para n>1
-#implemente um programa usando recursividade, isto é, uma função recursiva 
-#(loops simples serão desconsiderados) que calcula o n-ésimo elemento de uma série de Fibonacci. 
-#O valor de n deve ser obtido a partir de uma entrada do usuário. Imprima o resultado na tela
-
+.data
+input: .asciiz "Coloque um numero: \n"
+resultado: .asciiz "Fib "
+resultadoo: .asciiz " = "
+Breakline: .asciiz "\n"
 .text
 
 main:
 
 # Coloque um numero
-la $a0,comando   
-li $v0,4
+li $v0,4 # 
+la $a0,input # 
 syscall
-li $v0,5 #se lê o número(n)
-syscall
-move $t2,$v0 # Salva o número em t2
 
-# Chama a função de fibonacci
-move $a0,$t2 #a0 é colcoado em t2
-move $v0,$t2 # a0 é alocado para v0
+li $v0, 5 
+syscall
+
+move $a0,$v0 #a0 é colcoado em v0
+move $t2,$v0 # t2 é alocado para v0
 jal fib # fib (n)
 move $t3,$v0 #Resultado é armazenado em $t3
 
@@ -38,16 +24,20 @@ move $t3,$v0 #Resultado é armazenado em $t3
 la $a0,resultado #Printa fib
 li $v0,4 #Carrega na pilha
 syscall
+
 move $a0,$t2 #Printa o valor
 li $v0,1 #Carrega na pilha
 syscall
+
 la $a0,resultadoo #Print =
 li $v0,4 #Carrega na pilha
 syscall
+
 move $a0,$t3 #Print o resultado
 li $v0,1 #Carrega na pilha
 syscall
-la $a0,fim #Printa '\n'
+
+la $a0,Breakline #Printa '\n'
 li $v0,4 #Carrega na pilha
 syscall
 
@@ -56,43 +46,33 @@ li $v0,10 #Carrega na pilha
 syscall
 
 fib:
-# Relaciona e dá o resultado em fibonacci
-beqz $a0,zero #se n=0 return 0
-beq $a0,1,um #se n=1 return 1
+beq $a0, 0, igualzero # caso o valor digitado seja 0
+beq $a0,1, igualum # caso o valor digitado seja 1
+addi $sp, $sp, -12 # abre espaço na pilha 
+sw $ra, 8($sp) # escreve o endereço na sp
+sw $s0, 4($sp) # escreve o endereço na sp
+sw $s1, 0($sp) # escreve o endereço na sp
+move $s0, $a0 # coloca o valor de a0 em s0
+li $v0, 1 # return value for terminal condition
+ble $s0, 2, fim # check terminal condition
+addi $a0, $s0, -1 # set args for recursive call to f(n-1)
+jal fib
+move $s1, $v0 # store result of f(n-1) to s1
+addi $a0, $s0, -2 # set args for recursive call to f(n-2)
+jal fib
+add $v0, $s1, $v0 # add result of f(n-1) to it
+jal fim # Envia para o fim
 
-# fib(n-1)
-sub $sp,$sp,4 #Guarda o valor na pilha
-sw $ra,0($sp) #Guarda na pilha
-sub $a0,$a0,1 #n-1
-jal fib #fib(n-1)
-add $a0,$a0,1 #Realiza parte da soma
-lw $ra,0($sp)#Retorna o RA da pilha
-add $sp,$sp,4 #Incrementa na pilha 
-sub $sp,$sp,4 #Envia o valor para a pilha
-sw $v0,0($sp) #Guarda na pilha
-# fib(n-2)
-sub $sp,$sp,4 #Guarda o valor na pilha
-sw $ra,0($sp) #Guarda na pilha
-sub $a0,$a0,2 #n-2
-jal fib #fib(n-2)
-add $a0,$a0,2 #Realiza parte da soma
-lw $ra,0($sp) #Retorna o RA da pilha
-add $sp,$sp,4 #Incrementa na pilha
-#---------------
-lw $s7,0($sp) #Busca o RA da pilha
-add $sp,$sp,4 #Incrementa na pilha
-add $v0,$v0,$s7 # f(n - 2)+fib(n-1)
-jr $ra # Decrementa e pega o próximo valor da pilha
-
-zero:
+igualzero:
 li $v0,0 #Torna 0
 jr $ra #Retorna 0 ao return address
-um:
-li $v0,1 #torna 1
-jr $ra #Retorna 1 ao return address
+igualum:
+li $v0,1 #Torna 1
+jr $ra # Retorna 1 ao return address
 
-.data
-comando: .asciiz "Coloque um numero: \n"
-resultado: .asciiz "Fib "
-resultadoo: .asciiz " = "
-fim: .asciiz "\n"
+fim:
+lw $ra, 8($sp) # recuperando ra
+lw $s0, 4($sp) # recuperando s0
+lw $s1, 0($sp) # recuperando s1
+addi $sp, $sp, 12 # retomando na pilha
+jr $ra # Retorna o resultado ao return address
